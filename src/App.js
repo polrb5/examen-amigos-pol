b9a058a5da7ceee8c5cabcb44ca6ec2ee23cfd57
Pol Reig <polreigbroto@gmail.com>
2021-06-22 15:42:45 +0200

    Puesta a punto

--- src/App.js
+++ src/App.js
@@ -1,3 +1,11 @@
+// import { FaPlusCircle, FaTimesCircle, FaTimes } from "react-icons/fa";
+// import {
+//   Redirect,
+//   Route,
+//   BrowserRouter as Router,
+//   Switch,
+// } from "react-router-dom";
+
 function App() {
   return <p>hola</p>;
 }
